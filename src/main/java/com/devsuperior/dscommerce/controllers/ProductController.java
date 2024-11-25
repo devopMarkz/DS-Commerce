2d4912848cd5ce271f187323d45092b2fb2453d7
devopMarkz <marcosdev2002@gmail.com>
2024-11-25 19:05:57 -0300

    Adicionando URI para informar Location na rota POST de ProductController

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 @CrossOrigin("*")
@@ -18,19 +23,28 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/{id}")
-    public ProductDTO findById(@PathVariable Long id){
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
         ProductDTO productDTO = productService.findById(id);
-        return productDTO;
+        return ResponseEntity.ok(productDTO);
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll(@PageableDefault(page = 0, size = 10) Pageable pageable){
-        return productService.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(@PageableDefault(page = 0, size = 10) Pageable pageable){
+        Page<ProductDTO> productDTOS = productService.findAll(pageable);
+        return ResponseEntity.ok(productDTOS);
     }
 
     @PostMapping
-    public ProductDTO insert(@RequestBody ProductDTO productDTO){
-        return productService.insert(productDTO);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO){
+        productDTO = productService.insert(productDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productDTO.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(productDTO);
     }
 
 }
